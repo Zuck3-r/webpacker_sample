cc3cd1a8657362f56a5946e4b864b4c319e3d984
Zuck3r <nakano.satoo@moneyforward.co.jp>
2021-03-20 14:36:59 +0900

    async_log

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_081928) do
+ActiveRecord::Schema.define(version: 2021_03_20_051726) do
+
+  create_table "async_logs", force: :cascade do |t|
+    t.string "message"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
